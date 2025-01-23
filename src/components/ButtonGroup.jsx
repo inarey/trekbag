bce9e8750cec9c21408d02c126e4fb9fb726e637
inarey <thoselovelywords@gmail.com>
2025-01-23 15:27:36 +0900

    Continue with the setup and styling

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,5 @@
+export default function ButtonGroup() {
+  return (
+    <div>ButtonGroup</div>
+  )
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,12 @@
+import AddItemForm from "./AddItemForm";
+import ButtonGroup from "./ButtonGroup";
+
 export default function Sidebar() {
   return (
-    <section>sidebar</section>
+    <div className="sidebar">
+      <AddItemForm />
+
+      <ButtonGroup /> 
+    </div>
   )
 }
