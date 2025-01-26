97323737ce0939254439d41e3b7e7a12e81bc6e0
inarey <thoselovelywords@gmail.com>
2025-01-26 09:17:13 +0900

    working on the itemslist...

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -1,5 +1,19 @@
 export default function ItemsList() {
   return (
-    <section>ItemsList</section>
+    <ul>
+      <Item item="item 1" />
+      <Item item="item 2" />
+      <Item item="item 3" />
+    </ul>
+  )
+}
+
+function Item () {
+  return (
+    <li className="item"> 
+      <label>
+        <input type="checkbox" />
+      </label>
+    </li>
   )
 }
