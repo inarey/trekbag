1a0c1b345f59b1812a8b7c85d8dec1a5817abd21
inarey <thoselovelywords@gmail.com>
2025-02-02 07:24:55 +0900

    implement autofocus on input

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "./Button";
 
 export default function AddItemForm({setItems}) {
   const [itemText, setItemText] = useState("")
-  return (
-    <form onSubmit={(event) => {
+  const inputRef = useRef()
+
+  const handleSubmit = (event) => {
       event.preventDefault()
+
+      // basic validation
+      if (!itemText) {
+        alert("Please enter an item")
+        inputRef.current.focus()
+        return
+      }
       const newItem = {
         id: new Date().getTime(),
         name: itemText,
         completed: false
       }
       setItems((prev) => [...prev, newItem])
-    } }>
+      setItemText("")
+    } 
+  
+  return (
+    <form onSubmit={handleSubmit}>
       <h2>Add an Item</h2>
-      <input type="text" value={itemText} onChange={(event) => {
-        setItemText(event.target.value)
-      }}/>
+      <input ref={inputRef} type="text" value={itemText} onChange={(event) => {
+        setItemText(event.target.value)  
+      }}autoFocus />
       <Button>Add to List</Button>
     </form>
   )
