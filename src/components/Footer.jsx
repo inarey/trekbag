46fe445bc6682b5a6dfce3b9b4a7de25f3d8d205
inarey <thoselovelywords@gmail.com>
2025-02-15 08:02:55 +0900

    start from scratch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <small>&copy; 2050. Copyright by ByteGrad.</small>
+      <small>&copy; 2050. Copyright by ByteGrad.Redesign by: <b>Shan-Ti</b></small>
       <p>
         Version <b>1.5</b>
       </p>
