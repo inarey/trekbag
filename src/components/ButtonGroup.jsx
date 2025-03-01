117bf3eb8680529e356403459efda293d2a077f4
inarey <thoselovelywords@gmail.com>
2025-03-02 06:58:53 +0900

    implement maps and keys for the secondary buttons

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,19 @@
 import Button from "./Button";
 
 export default function ButtonGroup({children}) {
+
+  const secondaryButtons = [
+    "Mark all as complete",
+    "Mark all as incomplete",
+    "Mark all as initial",
+    "Remove all items"
+  ]
+
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Mark all as initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {secondaryButtons.map(text => {
+        return <Button key={text} type="secondary">{text}</Button>
+      })}
     </section>
   )
 }
