2fcbadccb17fd2ad779b757ed53bb357e50036bb
inarey <thoselovelywords@gmail.com>
2025-03-03 06:24:08 +0900

    started working on the itemsList

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -1,5 +1,40 @@
+const initialItems = [
+  {
+    name: 'GoodMood',
+    packed: true,
+    id: 1,
+  },
+  {
+    name: 'Water',
+    packed: false,
+    id: 2,
+  },
+  {
+    name: 'Snacks',
+    packed: false,
+    id: 3,
+  },
+];
+
 export default function ItemsList() {
   return (
-    <div>ItemsList</div>
-  )
+    <div>
+      <ul>
+        {initialItems.map(item => {
+          return <Item key={item.id} item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className='item'>
+      <label>
+        <input type='checkbox' />
+        {item.name}
+      </label>
+    </li>
+  );
 }
